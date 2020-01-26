fc54e1a26c5e97cda8f9f470db65f4460b5e9f77
hosein <atashgahhosein@gmail.com>
2020-01-26 17:06:58 +0330

    fixet password and username problem

--- Start.sh
+++ Start.sh
@@ -12,18 +12,18 @@ sed  -i "12i  DB_DATABASE=$database" .env
 sed -i -e 's/DB_USERNAME=root//g' .env
 echo -n "Enter a  username > "
 read username
-sed  -i "12i  DB_DATABASE=$username" .env
+sed  -i "12i  DB_USERNAME=$username" .env
 
 # config password
 sed -i -e 's/DB_PASSWORD=//g' .env
 echo -n "Enter  password > "
 read password
-sed  -i "12i  DB_DATABASE=$password" .env
+sed  -i "12i  DB_PASSWORD=$password" .env
 
 echo "Server Ready"
 sudo php artisan serve &
-echo "Migration Started" 
+echo "Migration Started"
 sudo php artisan migrate &
-echo "migration Finished Successfuly" 
+echo "migration Finished Successfuly"
 echo "Queue Started"
 sudo php artisan queue:work --daemon --timeout=3000 &
