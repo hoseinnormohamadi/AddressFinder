3d1348569e0b51f0827b5c2422b148da63e4afd1
hosein <atashgahhosein@gmail.com>
2020-01-28 14:52:22 +0330

    added composer install command

--- Start.sh
+++ Start.sh
@@ -6,20 +6,21 @@ cp .env.example .env
 sed -i -e 's/DB_DATABASE=laravel//g' .env
 echo -n "Enter a database name > "
 read database
-sed  -i "12i  DB_DATABASE=$database" .env
+sed -i "12i  DB_DATABASE=$database" .env
 
 # config username
 sed -i -e 's/DB_USERNAME=root//g' .env
 echo -n "Enter a  username > "
 read username
-sed  -i "12i  DB_USERNAME=$username" .env
+sed -i "12i  DB_USERNAME=$username" .env
 
 # config password
 sed -i -e 's/DB_PASSWORD=//g' .env
 echo -n "Enter  password > "
 read password
-sed  -i "12i  DB_PASSWORD=$password" .env
-
+sed -i "12i  DB_PASSWORD=$password" .env
+echo "Composer try to intsall project dependency"
+sudo composer install
 echo "Server Ready"
 sudo php artisan serve &
 echo "Migration Started"
